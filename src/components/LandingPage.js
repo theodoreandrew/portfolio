e089914142bc91f49af69620e1dcd435d23a176d
Theodore Andrew <theodoreandrew@Theodores-MacBook-Pro.local>
2020-12-19 23:15:12 -0800

    refactor using useRef for DOM selector

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,85 +1,85 @@
-import React from "react";
+import React from 'react';
 
-import NavLanding from "./landing/NavLanding";
-import landingBg from "../images/landing-color2.jpeg";
+import NavLanding from './landing/NavLanding';
+import landingBg from '../images/landing-color2.jpeg';
 
-import "./LandingPage.css";
+import './LandingPage.css';
 
 const LandingPage = () => {
-  const toggleLandingNav = () => {
-    const burgerElements = document.querySelectorAll(".burger div");
-    const isActive = document
-      .querySelector(".burger")
-      .classList.contains("active");
+  // const burgerRef = useRef(null);
 
-    for (let i = 0; i < burgerElements.length; i++) {
-      if (i % 2 === 0) {
-        burgerElements[i].style.transitionDelay = isActive ? "0.3s" : "0s";
-      } else {
-        burgerElements[i].style.transitionDelay = !isActive ? "0.3s" : "0s";
-      }
-    }
+  // const toggleLandingNav = () => {
+  //   const isActive = burgerRef.current.classList.contains('active');
+  //   const burgerElements = burgerRef.current.childNodes;
 
-    document.querySelector(".burger").classList.toggle("active");
+  //   for (let i = 0; i < burgerElements.length; i++) {
+  //     if (i % 2 === 0) {
+  //       burgerElements[i].style.transitionDelay = isActive ? '0.3s' : '0s';
+  //     } else {
+  //       burgerElements[i].style.transitionDelay = !isActive ? '0.3s' : '0s';
+  //     }
+  //   }
 
-    document
-      .querySelector(".nav-landing-container")
-      .classList.toggle("nav-landing-active");
-    setTimeout(() => eachNavLinkAppear(), 400);
-  };
+  //   burgerRef.current.classList.toggle('active');
 
-  const eachNavLinkAppear = () => {
-    const navImgLinks = [...document.querySelectorAll(".nav-img")];
-    const navTxtLinks = [...document.querySelectorAll(".nav-text")];
+  //   document
+  //     .querySelector('.nav-landing-container')
+  //     .classList.toggle('nav-landing-active');
+  //   setTimeout(() => eachNavLinkAppear(), 400);
+  // };
 
-    for (let i = 0; i < navImgLinks.length; i++) {
-      navImgLinks[i].classList.toggle("nav-img-appear");
-    }
+  // const eachNavLinkAppear = () => {
+  //   const navImgLinks = [...document.querySelectorAll('.nav-img')];
+  //   const navTxtLinks = [...document.querySelectorAll('.nav-text')];
 
-    for (let i = 0; i < navTxtLinks.length; i++) {
-      setTimeout(
-        () => navTxtLinks[i].classList.toggle("nav-text-appear"),
-        200 * (i + 1)
-      );
-    }
-  };
+  //   for (let i = 0; i < navImgLinks.length; i++) {
+  //     navImgLinks[i].classList.toggle('nav-img-appear');
+  //   }
+
+  //   for (let i = 0; i < navTxtLinks.length; i++) {
+  //     setTimeout(
+  //       () => navTxtLinks[i].classList.toggle('nav-text-appear'),
+  //       200 * (i + 1)
+  //     );
+  //   }
+  // };
 
   return (
-    <div className="landing-container">
-      <div className="landing-bg" src={landingBg} alt="Theodore Andrew"></div>
+    <div className='landing-container'>
+      <div className='landing-bg' src={landingBg} alt='Theodore Andrew'></div>
 
-      <div className="burger" onClick={toggleLandingNav}>
-        <div className="line-1">
-          <div className="line-inner-1"></div>
+      {/* <div className='burger' ref={burgerRef} onClick={toggleLandingNav}>
+        <div className='line-1'>
+          <div className='line-inner-1'></div>
         </div>
-        <div className="line-2">
-          <div className="line-inner-2"></div>
+        <div className='line-2'>
+          <div className='line-inner-2'></div>
         </div>
-      </div>
+      </div> */}
       <NavLanding />
 
-      <div className="welcome-msg">
-        <div className="welcome-container">
-          <h1 className="welcome-name">
+      <div className='welcome-msg'>
+        <div className='welcome-container'>
+          <h1 className='welcome-name'>
             Theodore
             <br /> Andrew
           </h1>
-          <h3 className="role">Frontend Engineer</h3>
-          <p className="motto">
+          <h3 className='role'>Frontend Engineer</h3>
+          <p className='motto'>
             <em>
-              <span className="hobby">
-                <i class="fas fa-laptop-code"></i> Enthusiast Coder{" "}
-                <i class="fas fa-laptop-code"></i>
+              <span className='hobby'>
+                <i className='fas fa-laptop-code'></i> Enthusiast Coder{' '}
+                <i className='fas fa-laptop-code'></i>
                 <br />
-                <i class="fas fa-table-tennis"></i> Table Tennis Player{" "}
-                <i class="fas fa-table-tennis"></i>
+                <i className='fas fa-table-tennis'></i> Table Tennis Player{' '}
+                <i className='fas fa-table-tennis'></i>
                 <br />
-                <i class="fas fa-basketball-ball"></i> Ball for life{" "}
-                <i class="fas fa-basketball-ball"></i>
+                <i className='fas fa-basketball-ball'></i> Ball for life{' '}
+                <i className='fas fa-basketball-ball'></i>
               </span>
               <br />
               <br />
-              <span className="qotd">
+              <span className='qotd'>
                 "The same everlasting God who cares for you today will take care
                 of you tomorrow and every day. Either God will shield you from
                 suffering or will give you unfailing strength to bear it. Be at
@@ -87,35 +87,35 @@ const LandingPage = () => {
               </span>
             </em>
           </p>
-          <div className="contact-me">
-            <div className="contact-container">
-              <div className="contact-links">
+          <div className='contact-me'>
+            <div className='contact-container'>
+              <div className='contact-links'>
                 <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://www.linkedin.com/in/theodore-andrew/"
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href='https://www.linkedin.com/in/theodore-andrew/'
                 >
-                  <i className="fab fa-linkedin"></i>
+                  <i className='fab fa-linkedin'></i>
                 </a>
               </div>
-              <div className="vertical-line"></div>
-              <div className="contact-links">
+              <div className='vertical-line'></div>
+              <div className='contact-links'>
                 <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://www.facebook.com/theodore.andrew"
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href='https://www.facebook.com/theodore.andrew'
                 >
-                  <i className="fab fa-facebook-square"></i>
+                  <i className='fab fa-facebook-square'></i>
                 </a>
               </div>
-              <div className="vertical-line"></div>
-              <div className="contact-links">
+              <div className='vertical-line'></div>
+              <div className='contact-links'>
                 <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://github.com/theodoreandrew"
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href='https://github.com/theodoreandrew'
                 >
-                  <i className="fab fa-github"></i>
+                  <i className='fab fa-github'></i>
                 </a>
               </div>
             </div>
--- src/components/landing/NavLanding.js
+++ src/components/landing/NavLanding.js
@@ -1,38 +1,85 @@
-import React from "react";
+import React, { useRef } from 'react';
 
-import about from "../../images/about.jpeg";
-import project from "../../images/project2.jpeg";
-import resume from "../../images/resume.jpeg";
-import "./NavLanding.css";
+import about from '../../images/about.jpeg';
+import project from '../../images/project2.jpeg';
+import resume from '../../images/resume.jpeg';
+import './NavLanding.css';
 
 const NavLanding = () => {
+  const burgerRef = useRef(null);
+  const navLandingContainerRef = useRef(null);
+
+  const toggleLandingNav = () => {
+    const isActive = burgerRef.current.classList.contains('active');
+    const burgerElements = burgerRef.current.children;
+
+    for (let i = 0; i < burgerElements.length; i++) {
+      if (i % 2 === 0) {
+        burgerElements[i].style.transitionDelay = isActive ? '0.3s' : '0s';
+      } else {
+        burgerElements[i].style.transitionDelay = !isActive ? '0.3s' : '0s';
+      }
+    }
+
+    burgerRef.current.classList.toggle('active');
+    navLandingContainerRef.current.classList.toggle('nav-landing-active');
+
+    setTimeout(() => eachNavLinkAppear(), 400);
+  };
+
+  const eachNavLinkAppear = () => {
+    const navLinksDum = [...navLandingContainerRef.current.children];
+    const navImgDums = [];
+    const navTxtDums = [];
+
+    for (let i = 0; i < navLinksDum.length; i++) {
+      navImgDums.push(navLinksDum[i].children[0]);
+      navTxtDums.push(navLinksDum[i].children[1]);
+    }
+
+    for (let i = 0; i < navImgDums.length; i++) {
+      navImgDums[i].classList.toggle('nav-img-appear');
+    }
+
+    for (let i = 0; i < navTxtDums.length; i++) {
+      setTimeout(
+        () => navTxtDums[i].classList.toggle('nav-text-appear'),
+        200 * (i + 1)
+      );
+    }
+  };
+
   return (
-    <div className="nav-landing-container">
-      <div className="nav-landing-links">
-        <div className="links-inner">
-          <img src={about} className="nav-img" alt="about" />
-          <div className="nav-text">
-            <span className="nav-name">About</span>
+    <>
+      <div className='burger' ref={burgerRef} onClick={toggleLandingNav}>
+        <div className='line-1'>
+          <div className='line-inner-1'></div>
+        </div>
+        <div className='line-2'>
+          <div className='line-inner-2'></div>
+        </div>
+      </div>
+      <div className='nav-landing-container' ref={navLandingContainerRef}>
+        <div className='nav-landing-links'>
+          <img src={about} className='nav-img' alt='about' />
+          <div className='nav-text'>
+            <span className='nav-name'>About</span>
+          </div>
+        </div>
+        <div className='nav-landing-links'>
+          <img src={project} className='nav-img' alt='project' />
+          <div className='nav-text'>
+            <span className='nav-name'>Projects</span>
+          </div>
+        </div>
+        <div className='nav-landing-links resume-link'>
+          <img src={resume} className='nav-img' alt='resume' />
+          <div className='nav-text'>
+            <span className='nav-name'>Resume</span>
           </div>
         </div>
       </div>
-      <div className="nav-landing-links">
-        <div className="links-inner">
-          <img src={project} className="nav-img" alt="project" />
-          <div className="nav-text">
-            <span className="nav-name">Projects</span>
-          </div>
-        </div>
-      </div>
-      <div className="nav-landing-links resume-link">
-        <div className="links-inner">
-          <img src={resume} className="nav-img" alt="resume" />
-          <div className="nav-text">
-            <span className="nav-name">Resume</span>
-          </div>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
