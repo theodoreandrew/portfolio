9e99f38e0cdf73d3f251fce4b1e3554d21058825
Theodore Andrew <theodoreandrew@Theodores-MacBook-Pro.local>
2021-01-10 01:01:39 -0800

    change burger to use ref

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,56 +6,9 @@ import landingBg from '../images/landing-color2.jpeg';
 import './LandingPage.css';
 
 const LandingPage = () => {
-  // const burgerRef = useRef(null);
-
-  // const toggleLandingNav = () => {
-  //   const isActive = burgerRef.current.classList.contains('active');
-  //   const burgerElements = burgerRef.current.childNodes;
-
-  //   for (let i = 0; i < burgerElements.length; i++) {
-  //     if (i % 2 === 0) {
-  //       burgerElements[i].style.transitionDelay = isActive ? '0.3s' : '0s';
-  //     } else {
-  //       burgerElements[i].style.transitionDelay = !isActive ? '0.3s' : '0s';
-  //     }
-  //   }
-
-  //   burgerRef.current.classList.toggle('active');
-
-  //   document
-  //     .querySelector('.nav-landing-container')
-  //     .classList.toggle('nav-landing-active');
-  //   setTimeout(() => eachNavLinkAppear(), 400);
-  // };
-
-  // const eachNavLinkAppear = () => {
-  //   const navImgLinks = [...document.querySelectorAll('.nav-img')];
-  //   const navTxtLinks = [...document.querySelectorAll('.nav-text')];
-
-  //   for (let i = 0; i < navImgLinks.length; i++) {
-  //     navImgLinks[i].classList.toggle('nav-img-appear');
-  //   }
-
-  //   for (let i = 0; i < navTxtLinks.length; i++) {
-  //     setTimeout(
-  //       () => navTxtLinks[i].classList.toggle('nav-text-appear'),
-  //       200 * (i + 1)
-  //     );
-  //   }
-  // };
-
   return (
     <div className='landing-container'>
       <div className='landing-bg' src={landingBg} alt='Theodore Andrew'></div>
-
-      {/* <div className='burger' ref={burgerRef} onClick={toggleLandingNav}>
-        <div className='line-1'>
-          <div className='line-inner-1'></div>
-        </div>
-        <div className='line-2'>
-          <div className='line-inner-2'></div>
-        </div>
-      </div> */}
       <NavLanding />
 
       <div className='welcome-msg'>
--- src/components/landing/NavLanding.js
+++ src/components/landing/NavLanding.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 import about from '../../images/about.jpeg';
 import project from '../../images/project2.jpeg';
@@ -6,44 +6,42 @@ import resume from '../../images/resume.jpeg';
 import './NavLanding.css';
 
 const NavLanding = () => {
-  const burgerRef = useRef(null);
-  const navLandingContainerRef = useRef(null);
+  const burRef = useRef(null);
+  const burDiv1Ref = useRef(null);
+  const burDiv2Ref = useRef(null);
+  const burDivLine1Ref = useRef(null);
+  const burDivLine2Ref = useRef(null);
+  const [active, setActive] = useState(false);
 
   const toggleLandingNav = () => {
-    const isActive = burgerRef.current.classList.contains('active');
-    const burgerElements = burgerRef.current.children;
+    const burDiv1 = burDiv1Ref.current;
+    const burDiv2 = burDiv2Ref.current;
+    const burDivLine1 = burDivLine1Ref.current;
+    const burDivLine2 = burDivLine2Ref.current;
 
-    for (let i = 0; i < burgerElements.length; i++) {
-      if (i % 2 === 0) {
-        burgerElements[i].style.transitionDelay = isActive ? '0.3s' : '0s';
-      } else {
-        burgerElements[i].style.transitionDelay = !isActive ? '0.3s' : '0s';
-      }
-    }
+    setActive((prevState) => !prevState);
+    burDiv1.style.transitionDelay = active ? '0.3s' : '0s';
+    burDiv2.style.transitionDelay = active ? '0.3s' : '0s';
+    burDivLine1.style.transitionDelay = !active ? '0.3s' : '0s';
+    burDivLine2.style.transitionDelay = !active ? '0.3s' : '0s';
 
-    burgerRef.current.classList.toggle('active');
-    navLandingContainerRef.current.classList.toggle('nav-landing-active');
-
+    document
+      .querySelector('.nav-landing-container')
+      .classList.toggle('nav-landing-active');
     setTimeout(() => eachNavLinkAppear(), 400);
   };
 
   const eachNavLinkAppear = () => {
-    const navLinksDum = [...navLandingContainerRef.current.children];
-    const navImgDums = [];
-    const navTxtDums = [];
+    const navImgLinks = [...document.querySelectorAll('.nav-img')];
+    const navTxtLinks = [...document.querySelectorAll('.nav-text')];
 
-    for (let i = 0; i < navLinksDum.length; i++) {
-      navImgDums.push(navLinksDum[i].children[0]);
-      navTxtDums.push(navLinksDum[i].children[1]);
+    for (let i = 0; i < navImgLinks.length; i++) {
+      navImgLinks[i].classList.toggle('nav-img-appear');
     }
 
-    for (let i = 0; i < navImgDums.length; i++) {
-      navImgDums[i].classList.toggle('nav-img-appear');
-    }
-
-    for (let i = 0; i < navTxtDums.length; i++) {
+    for (let i = 0; i < navTxtLinks.length; i++) {
       setTimeout(
-        () => navTxtDums[i].classList.toggle('nav-text-appear'),
+        () => navTxtLinks[i].classList.toggle('nav-text-appear'),
         200 * (i + 1)
       );
     }
@@ -51,31 +49,41 @@ const NavLanding = () => {
 
   return (
     <>
-      <div className='burger' ref={burgerRef} onClick={toggleLandingNav}>
-        <div className='line-1'>
-          <div className='line-inner-1'></div>
+      <div
+        className={`burger ${active ? 'active' : ''}`}
+        onClick={toggleLandingNav}
+        ref={burRef}
+      >
+        <div className='line-1' ref={burDiv1Ref}>
+          <div className='line-inner-1' ref={burDivLine1Ref}></div>
         </div>
-        <div className='line-2'>
-          <div className='line-inner-2'></div>
+        <div className='line-2' ref={burDiv2Ref}>
+          <div className='line-inner-2' ref={burDivLine2Ref}></div>
         </div>
       </div>
-      <div className='nav-landing-container' ref={navLandingContainerRef}>
+      <div className='nav-landing-container'>
         <div className='nav-landing-links'>
-          <img src={about} className='nav-img' alt='about' />
-          <div className='nav-text'>
-            <span className='nav-name'>About</span>
+          <div className='links-inner'>
+            <img src={about} className='nav-img' alt='about' />
+            <div className='nav-text'>
+              <span className='nav-name'>About</span>
+            </div>
           </div>
         </div>
         <div className='nav-landing-links'>
-          <img src={project} className='nav-img' alt='project' />
-          <div className='nav-text'>
-            <span className='nav-name'>Projects</span>
+          <div className='links-inner'>
+            <img src={project} className='nav-img' alt='project' />
+            <div className='nav-text'>
+              <span className='nav-name'>Projects</span>
+            </div>
           </div>
         </div>
         <div className='nav-landing-links resume-link'>
-          <img src={resume} className='nav-img' alt='resume' />
-          <div className='nav-text'>
-            <span className='nav-name'>Resume</span>
+          <div className='links-inner'>
+            <img src={resume} className='nav-img' alt='resume' />
+            <div className='nav-text'>
+              <span className='nav-name'>Resume</span>
+            </div>
           </div>
         </div>
       </div>
