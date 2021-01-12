eb0210bbcb2c4974bc9321a7043e88deea6a2996
Theodore Andrew <theodoreandrew@Theodores-MacBook-Pro.local>
2021-01-11 22:06:55 -0800

    add landing page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { LandingPage } from './LandingPage';
+import './App.css';
 
 export const App = () => {
   return (
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import './LandingPage.css';
+
 export const LandingPage = () => {
   return (
     <div>
-      <h1>LandingPage</h1>
+      <div className='landing-bg'></div>
+      <div className='section1'>
+        <div className='greetings'>Hello World, I'm</div>
+        <div className='full-name-container'>
+          <div className='full-name'>Theodore Andrew</div>
+        </div>
+      </div>
     </div>
   );
 };
